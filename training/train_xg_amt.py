f78c434c41ffcb5c7fc3023c761548f4b9d4b19b
Munish Dabra <100611213+awsdabra@users.noreply.github.com>
2023-01-12 14:37:57 -0600

    Updated training script to remove S3 hardcoding
    Updated training script to remove S3 hardcoding

--- training/train_xg_amt.py
+++ training/train_xg_amt.py
@@ -9,10 +9,6 @@ import numpy as np
 import xgboost
 import statistics
 import boto3
-
-# from sagemaker_training import environment
-
-# import dmatrix2np as dmatrix2np
 from dmatrix2np import dmatrix_to_numpy
 from sklearn.metrics import roc_auc_score
 
@@ -43,42 +39,33 @@ def parse_args():
 
     return args
 
-# TODO: REMOVE ME : Cleanup
 def write_to_s3(di, auc_score):
-
     args = parse_args()
-    
-    # Save a text file to output_data_dir
+    # Save a temp text file to output_data_dir
     f = open(os.path.join(args.output_data_dir, 'raw_ouptut.txt'), 'a+')
     f.write(str(di) + "," + str(auc_score) + "\n")
     f.close()
             
 def eval_auc_score(predt, dtrain):
-
     fY = [1 if p > 0.5 else 0 for p in predt]
     y = dtrain.get_label()
     auc_score = roc_auc_score(y, fY)
+    
     return auc_score
 
-# Combined metrics for DI and AUC
+# Combined metrics for SD and AUC
 def eval_combined_metric(predt, dtrain):
-  
     auc_score = eval_auc_score(predt, dtrain)
-    di = eval_disparate_impact(predt, dtrain)
-    #Maximize (1-DI) for the purpose of minimizing DI 
-    combined_metric = ((4*auc_score)+(1-di))/5
-    #combined_metric = round(statistics.mean([auc_score, 1-di]), 4)
-       
-    print("Statistical Disparity, AUC Score, Combined Metric: ", di, auc_score, combined_metric)
+    sd = eval_statistical_disparity(predt, dtrain)
+    combined_metric = ((4*auc_score)+(1-sd))/5
+    print("Statistical Disparity, AUC Score, Combined Metric: ", sd, auc_score, combined_metric)
+    write_to_s3(round(sd,4), round(auc_score,4))
     
-    # TODO: REMOVE ME : Cleanup
-    write_to_s3(round(di,4), round(auc_score,4))
-        
     return "auc", combined_metric
 
-def eval_disparate_impact(predt, dtrain):
+def eval_statistical_disparity(predt, dtrain):
     """
-    Eval DI metric 
+    Eval SD metric 
     fY - prediction [ credit risk - 0: bad 1: good]
     groups - Foreign worker [ 1 yes, 2 no]
     """
@@ -89,9 +76,7 @@ def eval_disparate_impact(predt, dtrain):
     groups = [item for sublist in dtrain_np[:, -1:] for item in sublist]
     
     fY = [1 if p > 0.5 else 0 for p in predt]
-    
     sp = [0, 0]
-    
     sp[0] = float(
         len([1 for idx, fy in enumerate(fY) if fy == 1 and groups[idx] == 1.0])
     ) / len([1 for idx, fy in enumerate(fY) if groups[idx] == 1.0])
@@ -100,15 +85,14 @@ def eval_disparate_impact(predt, dtrain):
         len([1 for idx, fy in enumerate(fY) if fy == 1 and groups[idx] == 2.0])
     ) / len([1 for idx, fy in enumerate(fY) if groups[idx] == 2.0])
     
-    di = abs(sp[0] - sp[1])
+    sd = abs(sp[0] - sp[1])
     
-    return di
+    return sd
     
 
 def main():
 
     args = parse_args()
-    print(args)
     
     train_files_path, validation_files_path = args.train, args.validation
 
@@ -118,11 +102,11 @@ def main():
     val_features_path = os.path.join(args.validation, "val_features.csv")
     val_labels_path = os.path.join(args.validation, "val_labels.csv")
 
-    print("Loading training dataframes...")
+    #Loading training dataframes
     df_train_features = pd.read_csv(train_features_path)
     df_train_labels = pd.read_csv(train_labels_path)
 
-    print("Loading validation dataframes...")
+    #Loading validation dataframes
     df_val_features = pd.read_csv(val_features_path)
     df_val_labels = pd.read_csv(val_labels_path)
 
@@ -154,10 +138,9 @@ def main():
         params=params, dtrain=dtrain, feval=eval_combined_metric, evals=watchlist, num_boost_round=args.num_round, evals_result=evals_result
     )
     
-    
-    ##################
-    # TODO: REMOVE ME : Cleanup
-    
+    # Write the results to S3 bucket (optional) - only to plot 'Pareto frontier'
+    import uuid
+    import re
     raw_df = pd.read_csv(args.output_data_dir + 'raw_ouptut.txt',  sep=',', header = None)
     raw_file_name = 'raw_ouptut.txt'
     sorted_file_name = "sorted_output.txt"
@@ -165,18 +148,17 @@ def main():
     # get last row
     df = df.iloc[-1:]
     df.to_csv(args.output_data_dir + sorted_file_name, sep=',', index=False, header=False)
-   
-    import uuid
-    random_id = str(uuid.uuid4().hex)[:8]
-    out_file_name = '{}-output.txt'.format(random_id)
     
-    s3_bucket_name = "sagemaker-us-east-2-921553072635" # Update S3 bucket name and prefix
-    s3_bucket_prefix = "sagemaker/sagemaker-amt-credit-risk-model/data/output/v4/"
+    random_id = str(uuid.uuid4().hex)[:8]                                            
+    out_file_name = '{}-output.txt'.format(random_id)               
+
+    s3_module_dir = os.environ.get("SM_MODULE_DIR")
+    tmp_s3_bucket_path = s3_module_dir.replace("s3://","")
+    s3_bucket_name = re.split('[/]',tmp_s3_bucket_path)[0]
+    s3_bucket_prefix = "sagemaker/sagemaker-amt-credit-risk-model/data/output/tmp/"
     
     with open(args.output_data_dir + sorted_file_name, "rb") as data:
         s3.upload_fileobj(data, s3_bucket_name, s3_bucket_prefix + out_file_name)
-        
-    ##################
     
     # Write model
     model_dir = os.environ.get("SM_MODEL_DIR")
